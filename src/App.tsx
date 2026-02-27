3e73d40b8b69099680c28ce3e35efe9a24c692bc
Maulei <maulei@connect.hku.hk>
2026-02-27 13:24:54 +0800

    Add basic UI skeleton for todo app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex flex-col items-center pt-10">
+      <h1 className="text-4xl font-bold">Todo App</h1>
+    </div>
+  )
+}
+
+export default App
